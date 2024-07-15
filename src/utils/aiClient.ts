b96660ba4f343c87443e281deaaa6e84274d889d
wuchuheng <root@wuchuheng.com>
2024-07-16 03:46:02 +0800

    feat(ai-client): Improve AI request handling and logging   - Implement async request to OpenAI API with dynamic secret key retrieval.   - Add detailed logging for token usage and fees.

--- src/utils/aiClient.ts
+++ src/utils/aiClient.ts
@@ -1,10 +1,13 @@
 import { API_URL, getSecretKey, MODEL_ID } from "../config/config";
 import { ChatCompletionMessageParam } from "openai/resources/chat/completions.mjs";
 import axios from "axios";
+import LogUtil from "./logUtil";
 
 const AiRequest = async (
   messages: Array<ChatCompletionMessageParam>
 ): Promise<string> => {
+  // 2. Processing logic.
+  // 2.1 Send the request to the OpenAI API, and get the response from the API.
   const secretKey = await getSecretKey();
   const response = await axios.post(
     `${API_URL}`,
@@ -17,6 +20,18 @@ const AiRequest = async (
     }
   );
   const content = response.data.choices[0].message.content;
+
+  // 2.2 Fees used under statistics
+  const { completion_tokens, prompt_tokens, total_tokens } =
+    response.data.usage;
+  const totalFeel = ((total_tokens / 1000) * 0.0001)
+    .toString()
+    .padStart(6, "0");
+  LogUtil.debug(
+    `Token usage: completion_tokens: ${completion_tokens} prompt_tokens: ${prompt_tokens} total_tokens: ${total_tokens} Fee: ${totalFeel} RMB `
+  );
+
+  // 3. Return the result.
 
   return content;
 };
